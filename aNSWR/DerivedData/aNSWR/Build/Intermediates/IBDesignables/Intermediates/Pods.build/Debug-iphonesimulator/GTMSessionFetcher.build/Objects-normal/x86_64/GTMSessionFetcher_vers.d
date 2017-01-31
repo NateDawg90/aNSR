0848821116da54f0b7cbefe9c6a36b8b99906418
Hui Jun <hjyam@umich.edu>
2017-01-31 02:58:01 -0800

    Complete sign up and sign in page

---
+++ aNSWR/DerivedData/aNSWR/Build/Intermediates/IBDesignables/Intermediates/Pods.build/Debug-iphonesimulator/GTMSessionFetcher.build/Objects-normal/x86_64/GTMSessionFetcher_vers.d
@@ -0,0 +1,2 @@
+dependencies: \
+  /Users/huijun/Desktop/bootcamp/ios_project/ios-project/aNSWR/DerivedData/aNSWR/Build/Intermediates/IBDesignables/Intermediates/Pods.build/Debug-iphonesimulator/GTMSessionFetcher.build/DerivedSources/GTMSessionFetcher_vers.c
---
+++ aNSWR/DerivedData/aNSWR/Build/Intermediates/Pods.build/Debug-iphonesimulator/Pods-aNSWR.build/Objects-normal/x86_64/Pods_aNSWR_vers.d
@@ -0,0 +1,2 @@
+dependencies: \
+  /Users/huijun/Desktop/bootcamp/ios_project/ios-project/aNSWR/DerivedData/aNSWR/Build/Intermediates/Pods.build/Debug-iphonesimulator/Pods-aNSWR.build/DerivedSources/Pods_aNSWR_vers.c
