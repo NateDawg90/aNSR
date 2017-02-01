53f9766e7d104343ff1d52b14fd48b31091614a1
Nathan Johnson <rnj@clemson.edu>
2017-02-01 10:54:10 -0800

    Pull

--- aNSWR/DerivedData/aNSWR/Build/Intermediates/IBDesignables/Intermediates/Pods.build/Debug-iphonesimulator/Pods-aNSWR.build/Objects-normal/x86_64/Pods-aNSWR-dummy.d
+++
@@ -1,3 +0,0 @@
-dependencies: \
-  /Users/huijun/Desktop/bootcamp/ios_project/ios-project/aNSWR/Pods/Target\ Support\ Files/Pods-aNSWR/Pods-aNSWR-dummy.m \
-  /Users/huijun/Desktop/bootcamp/ios_project/ios-project/aNSWR/Pods/Firebase/Core/Sources/module.modulemap
--- aNSWR/DerivedData/aNSWR/Build/Intermediates/aNSWR.build/Debug-iphonesimulator/aNSWRTests.build/Script-51D52614C47EAEAF0F7076A1.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-"${SRCROOT}/Pods/Target Support Files/Pods-aNSWRTests/Pods-aNSWRTests-frameworks.sh"
-
