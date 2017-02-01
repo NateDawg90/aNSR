afd5dac91cff4e171db3b8be6ed81694b78f1b2d
Hui Jun <hjyam@umich.edu>
2017-02-01 15:18:20 -0800

    Create DetailsVC class and QuestionCustonCell class

---
+++ aNSWR/aNSWR/QuestionCustomCell.swift
@@ -0,0 +1,24 @@
+//
+//  QuestionCustomCell.swift
+//  aNSWR
+//
+//  Created by Hui Jun on 2/1/17.
+//  Copyright © 2017 aNSWR. All rights reserved.
+//
+
+import UIKit
+
+class QuestionCustomCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
