91cc232583d7401d9290129fe819fd18c8ca58f0
Andrew Lim <admin@Admins-MBP.lan>
2017-01-31 23:50:44 -0800

    update question branch

--- aNSWR/aNSWR/CreateQuestionVC.swift
+++ aNSWR/aNSWR/CreateQuestionVC.swift
@@ -2,21 +2,31 @@
 //  CreateQuestionVC.swift
 //  aNSWR
 //
-//  Created by Andrew on 1/31/17.
+//  Created by Hui Jun on 1/31/17.
 //  Copyright © 2017 aNSWR. All rights reserved.
 //
 
 import UIKit
-import Firebase
+import FirebaseAuth
 
 class CreateQuestionVC: UIViewController {
-    
-    private let NEWS_FEED_SEGUE_ID = "NewsFeedVC"
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
     
     @IBOutlet weak var QuestionText: CustomTextField!
     
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    @IBAction func CreateQuestionButton(_ sender: AnyObject) {
+        if QuestionText.text != "" {
+            DBProvider.instance.saveQuestion(questionText: QuestionText.text!, userID: (FIRAuth.auth()?.currentUser?.uid)!)
+        }
     }
-    
+}
