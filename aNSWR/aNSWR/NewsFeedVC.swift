367200770f57541d48cabff9c3117045ce89618f
Hui Jun <hjyam@umich.edu>
2017-02-01 13:03:40 -0800

    Update

--- aNSWR/aNSWR/NewsFeedVC.swift
+++ aNSWR/aNSWR/NewsFeedVC.swift
@@ -27,26 +27,18 @@ class NewsFeedVC: UIViewController {
     @IBAction func LogOutButton(_ sender: AnyObject) {
         if AuthProvider.instance.logOut() {
             dismiss(animated: true, completion: nil);
+            
         } else {
             showAlertMessage(title: "Could Not Log Out", message: "We Have A Problem With Connecting To Database To Log Out, Please Try Again");
         }
+
     }
-    
+        
     private func showAlertMessage(title: String, message: String) {
         let alert = UIAlertController(title: title, message: message, preferredStyle: .alert);
         let ok = UIAlertAction(title: "Ok", style: .cancel, handler: nil);
         alert.addAction(ok);
         self.present(alert, animated: true, completion: nil);
     }
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
--- aNSWR/aNSWR/SplashVC.swift
+++ aNSWR/aNSWR/SplashVC.swift
@@ -10,6 +10,8 @@ import UIKit
 
 class SplashVC: UIViewController {
 
+//    private let NEWS_FEED_SEGUE_ID = "NewsFeedVC"
+
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
