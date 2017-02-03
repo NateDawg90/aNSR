65a438004aa096ec06821ec29ff2e6de0017caf2
Hui Jun <hjyam@umich.edu>
2017-02-03 12:11:06 -0800

    Display questions and answer on story board and finished loading data from db

--- aNSWR/aNSWR/aNSWR/DetailsVC.swift
+++ aNSWR/aNSWR/aNSWR/DetailsVC.swift
@@ -7,13 +7,34 @@
 //
 
 import UIKit
+import Firebase
 
 class DetailsVC: UIViewController {
     var questionText = String()
-    var answers = [String()]
+    var answers = [String]()
+    var questionID = String()
+    var votes = [Int]()
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        var ref: FIRDatabaseReference!
+        ref = FIRDatabase.database().reference()
+        ref.child("answers").observeSingleEvent(of: .value, with: { (snapshot) in
+            if let snapshots = snapshot.children.allObjects as? [FIRDataSnapshot] {
+                for answer in self.answers {
+                    for snap in snapshots {
+                        let value = snap.value as? NSDictionary
+                        if ((value?["answerText"] as? String == answer) &&
+                            (value?["questionID"] as? String == self.questionID)) {
+                            self.votes.append((value?["votes"] as? Int)!)
+                        }
+                    }
+                    
+                }
+
+            }
+
+        })
     }
 
     override func didReceiveMemoryWarning() {
@@ -21,9 +42,11 @@ class DetailsVC: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    
     @IBAction func backButton(_ sender: AnyObject) {
         dismiss(animated: true, completion: nil)
     }
     
+    
 }
+
