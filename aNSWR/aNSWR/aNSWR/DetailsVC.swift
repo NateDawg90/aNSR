0ac5def4a200a8a0d297dc70a293988422a9d8a7
Hui Jun <hjyam@umich.edu>
2017-02-03 05:01:00 -0800

    Complete data transfer from newsfeed to detailsVC

---
+++ aNSWR/aNSWR/aNSWR/DetailsVC.swift
@@ -0,0 +1,27 @@
+//
+//  DetailsVC.swift
+//  aNSWR
+//
+//  Created by Hui Jun on 2/1/17.
+//  Copyright © 2017 aNSWR. All rights reserved.
+//
+
+import UIKit
+
+class DetailsVC: UIViewController {
+    var questionText = String()
+    var answers = [String()]
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        print("..................\(questionText)")
+        print(answers)
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
