5cd473b13372cdd15f489a1cda603199c8d6848e
Hui Jun <hjyam@umich.edu>
2017-02-04 09:34:39 -0800

    Complete voting

--- aNSWR/aNSWR/aNSWR/DetailsVC.swift
+++ aNSWR/aNSWR/aNSWR/DetailsVC.swift
@@ -48,7 +48,6 @@ class DetailsVC: UIViewController {
             }
   
             self.changeButtonSign()
-            print("....................................")
             print(self.didVoteAns)
         })
     }
@@ -91,59 +90,122 @@ class DetailsVC: UIViewController {
         }
     }
     
-//    func updateData(questionID: String) {
-//        self.questionText.removeAll()
-//        self.answers.removeAll()
-//        self.votes.removeAll()
-//        self.didVoteAns.removeAll()
-//        
-//        var ref: FIRDatabaseReference!
-//        ref = FIRDatabase.database().reference()
-//        ref.child("questions").child(questionID).observeSingleEvent(of: .value, with: { (snapshot) in
-//        
-//            let value = snapshot.value as? NSDictionary
-//            self.questionText = value?["questionText"] as? String ?? ""
-//            self.answers = value?["answers"] as? Array<AnyObject> as! [String]? ?? [("" as AnyObject) as! String]
-//        })
-//        
-//        for answer in self.answers {
-//            ref.child("answers").observeSingleEvent(of: .value, with: { (snapshot) in
-//                if let snapshots = snapshot.children.allObjects as? [FIRDataSnapshot] {
-//                    for snap in snapshots {
-//                        for singleSnap in snapshot.children.allObjects {
-//                            let answer = singleSnap as! FIRDataSnapshot
-//                            self.answerKey = answer.key
-//                        }
-//                        let value = snap.value as? NSDictionary
-//                        if ((value?["answerText"] as? String == answer) &&
-//                            (value?["questionID"] as? String == self.questionID)) {
-//                            self.answersID.append(self.answerKey)
-//                            self.votes.append((value?["votes"] as? Int)!)
-//                            let voters = value?["answerVoters"] as? Array<String>
-//                            if (voters?.contains(self.loginUserID))! {
-//                                self.didVoteAns.append(true)
-//                            } else {
-//                                self.didVoteAns.append(false)
-//                            }
-//                        }
-//                    }
-//                }
-                print(".....................\(self.answersID)")
-    self.changeButtonSign()
+    func updateData(questionID: String) {
+        self.questionText.removeAll()
+        self.answers.removeAll()
+        self.votes.removeAll()
+        self.didVoteAns.removeAll()
+        self.answersID.removeAll()
+        self.voters.removeAll()
+        
+        var ref: FIRDatabaseReference!
+        ref = FIRDatabase.database().reference()
+        ref.child("answers").observeSingleEvent(of: .value, with: { (snapshot) in
+            //get answer key
+            for singleSnap in snapshot.children.allObjects {
+                let answer = singleSnap as! FIRDataSnapshot
+                self.answerKey = answer.key
+                
+                let answerSnap = singleSnap as? FIRDataSnapshot
+                let value = answerSnap?.value as? NSDictionary
+                if (value?["questionID"] as? String == self.questionID) {
+                    self.answersID.append(self.answerKey)
+                    self.votes.append((value?["votes"] as? Int)!)
+                    self.voters.append((value?["answerVoters"] as! [String]))
+                    let voters = value?["answerVoters"] as? Array<String>
+                    if (voters?.contains(self.loginUserID))! {
+                        self.didVoteAns.append(true)
+                    } else {
+                        self.didVoteAns.append(false)
+                    }
+                }
+            }
+            
+            self.changeButtonSign()
+        })
 
-//            })
-//
-//        }
-//    }
-//    
+   }
+    
     func changeButtonSign() {
-        for i in 0..<didVoteAns.count {
-            if (didVoteAns[i] == true) {
-                print("..............................")
-                print(didVoteAns[i])
+        if (didVoteAns.count == 1) {
+            if (didVoteAns[0] == true) {
                 answer1Button.setTitle("-", for: .normal)
             } else {
                 answer1Button.setTitle("+", for: .normal)
+            }
+        } else if (didVoteAns.count == 2) {
+            if (didVoteAns[0] == true) {
+                answer1Button.setTitle("-", for: .normal)
+            } else {
+                answer1Button.setTitle("+", for: .normal)
+            }
+            if (didVoteAns[1] == true) {
+                answer2Button.setTitle("-", for: .normal)
+            } else {
+                answer2Button.setTitle("+", for: .normal)
+            }
+        } else if (didVoteAns.count == 3) {
+            if (didVoteAns[0] == true) {
+                answer1Button.setTitle("-", for: .normal)
+            } else {
+                answer1Button.setTitle("+", for: .normal)
+            }
+            if (didVoteAns[1] == true) {
+                answer2Button.setTitle("-", for: .normal)
+            } else {
+                answer2Button.setTitle("+", for: .normal)
+            }
+            if (didVoteAns[2] == true) {
+                answer3Button.setTitle("-", for: .normal)
+            } else {
+                answer3Button.setTitle("+", for: .normal)
+            }
+        } else if (didVoteAns.count == 4) {
+            if (didVoteAns[0] == true) {
+                answer1Button.setTitle("-", for: .normal)
+            } else {
+                answer1Button.setTitle("+", for: .normal)
+            }
+            if (didVoteAns[1] == true) {
+                answer2Button.setTitle("-", for: .normal)
+            } else {
+                answer2Button.setTitle("+", for: .normal)
+            }
+            if (didVoteAns[2] == true) {
+                answer3Button.setTitle("-", for: .normal)
+            } else {
+                answer3Button.setTitle("+", for: .normal)
+            }
+            if (didVoteAns[3] == true) {
+                answer4Button.setTitle("-", for: .normal)
+            } else {
+                answer4Button.setTitle("+", for: .normal)
+            }
+        } else {
+            if (didVoteAns[0] == true) {
+                answer1Button.setTitle("-", for: .normal)
+            } else {
+                answer1Button.setTitle("+", for: .normal)
+            }
+            if (didVoteAns[1] == true) {
+                answer2Button.setTitle("-", for: .normal)
+            } else {
+                answer2Button.setTitle("+", for: .normal)
+            }
+            if (didVoteAns[2] == true) {
+                answer3Button.setTitle("-", for: .normal)
+            } else {
+                answer3Button.setTitle("+", for: .normal)
+            }
+            if (didVoteAns[3] == true) {
+                answer4Button.setTitle("-", for: .normal)
+            } else {
+                answer4Button.setTitle("+", for: .normal)
+            }
+            if (didVoteAns[4] == true) {
+                answer5Button.setTitle("-", for: .normal)
+            } else {
+                answer5Button.setTitle("+", for: .normal)
             }
         }
     }
@@ -205,29 +267,157 @@ class DetailsVC: UIViewController {
     
     @IBAction func answer1Button(_ sender: AnyObject) {
         if (answer1Button.titleLabel?.text == "+") {
-            answer1Button.setTitle("-", for: .normal)
-            self.voters[0].append(self.loginUserID)
-            DBProvider.instance.answerRef.child(self.answersID[0]).updateChildValues(["votes": (self.votes[0] + 1)])
-            DBProvider.instance.answerRef.child(self.answersID[0]).updateChildValues(["answerVoters": self.voters[0]])
+            let prevVote = self.didVoteAns.index(of: true)
+            if (prevVote == nil) {
+                answer1Button.setTitle("-", for: .normal)
+                self.voters[0].append(self.loginUserID)
+                DBProvider.instance.answerRef.child(self.answersID[0]).updateChildValues(["votes": (self.votes[0] + 1)])
+                DBProvider.instance.answerRef.child(self.answersID[0]).updateChildValues(["answerVoters": self.voters[0]])
+                updateData(questionID: self.questionID)
+            } else {
+                let userIdx = self.voters[prevVote!].index(of: "\(self.loginUserID)")
+                self.voters[prevVote!].remove(at: userIdx!)
+                answer1Button.setTitle("-", for: .normal)
+                self.voters[0].append(self.loginUserID)
+                DBProvider.instance.answerRef.child(self.answersID[prevVote!]).updateChildValues(["votes": (self.votes[prevVote!] - 1)])
+                DBProvider.instance.answerRef.child(self.answersID[prevVote!]).updateChildValues(["answerVoters": self.voters[prevVote!]])
+                DBProvider.instance.answerRef.child(self.answersID[0]).updateChildValues(["votes": (self.votes[0] + 1)])
+                DBProvider.instance.answerRef.child(self.answersID[0]).updateChildValues(["answerVoters": self.voters[0]])
+                updateData(questionID: self.questionID)
+            }
+            
         } else {
             answer1Button.setTitle("+", for: .normal)
             let index = self.voters[0].index(of: "\(self.loginUserID)")
             self.voters[0].remove(at: index!)
             DBProvider.instance.answerRef.child(self.answersID[0]).updateChildValues(["votes": (self.votes[0] - 1)])
             DBProvider.instance.answerRef.child(self.answersID[0]).updateChildValues(["answerVoters": self.voters[0]])
+            updateData(questionID: self.questionID)
         }
     }
     
     @IBAction func answer2Button(_ sender: AnyObject) {
+        if (answer2Button.titleLabel?.text == "+") {
+            let prevVote = self.didVoteAns.index(of: true)
+            if (prevVote == nil) {
+                answer2Button.setTitle("-", for: .normal)
+                self.voters[1].append(self.loginUserID)
+                DBProvider.instance.answerRef.child(self.answersID[1]).updateChildValues(["votes": (self.votes[1] + 1)])
+                DBProvider.instance.answerRef.child(self.answersID[1]).updateChildValues(["answerVoters": self.voters[1]])
+                updateData(questionID: self.questionID)
+            } else {
+                let userIdx = self.voters[prevVote!].index(of: "\(self.loginUserID)")
+                self.voters[prevVote!].remove(at: userIdx!)
+                answer2Button.setTitle("-", for: .normal)
+                self.voters[1].append(self.loginUserID)
+                DBProvider.instance.answerRef.child(self.answersID[prevVote!]).updateChildValues(["votes": (self.votes[prevVote!] - 1)])
+                DBProvider.instance.answerRef.child(self.answersID[prevVote!]).updateChildValues(["answerVoters": self.voters[prevVote!]])
+                DBProvider.instance.answerRef.child(self.answersID[1]).updateChildValues(["votes": (self.votes[1] + 1)])
+                DBProvider.instance.answerRef.child(self.answersID[1]).updateChildValues(["answerVoters": self.voters[1]])
+                updateData(questionID: self.questionID)
+            }
+        } else {
+            answer2Button.setTitle("+", for: .normal)
+            let index = self.voters[1].index(of: "\(self.loginUserID)")
+            self.voters[1].remove(at: index!)
+            DBProvider.instance.answerRef.child(self.answersID[1]).updateChildValues(["votes": (self.votes[1] - 1)])
+            DBProvider.instance.answerRef.child(self.answersID[1]).updateChildValues(["answerVoters": self.voters[1]])
+            updateData(questionID: self.questionID)
+        }
+
     }
     
     @IBAction func answer3Button(_ sender: AnyObject) {
+        if (answer3Button.titleLabel?.text == "+") {
+            let prevVote = self.didVoteAns.index(of: true)
+            if (prevVote == nil) {
+                answer3Button.setTitle("-", for: .normal)
+                self.voters[2].append(self.loginUserID)
+                DBProvider.instance.answerRef.child(self.answersID[2]).updateChildValues(["votes": (self.votes[2] + 1)])
+                DBProvider.instance.answerRef.child(self.answersID[2]).updateChildValues(["answerVoters": self.voters[2]])
+                updateData(questionID: self.questionID)
+            } else {
+                let userIdx = self.voters[prevVote!].index(of: "\(self.loginUserID)")
+                self.voters[prevVote!].remove(at: userIdx!)
+                answer3Button.setTitle("-", for: .normal)
+                self.voters[2].append(self.loginUserID)
+                DBProvider.instance.answerRef.child(self.answersID[prevVote!]).updateChildValues(["votes": (self.votes[prevVote!] - 1)])
+                DBProvider.instance.answerRef.child(self.answersID[prevVote!]).updateChildValues(["answerVoters": self.voters[prevVote!]])
+                DBProvider.instance.answerRef.child(self.answersID[2]).updateChildValues(["votes": (self.votes[2] + 1)])
+                DBProvider.instance.answerRef.child(self.answersID[2]).updateChildValues(["answerVoters": self.voters[2]])
+                updateData(questionID: self.questionID)
+            }
+        } else {
+            answer3Button.setTitle("+", for: .normal)
+            let index = self.voters[2].index(of: "\(self.loginUserID)")
+            self.voters[2].remove(at: index!)
+            DBProvider.instance.answerRef.child(self.answersID[2]).updateChildValues(["votes": (self.votes[2] - 1)])
+            DBProvider.instance.answerRef.child(self.answersID[2]).updateChildValues(["answerVoters": self.voters[2]])
+            updateData(questionID: self.questionID)
+        }
     }
     
     @IBAction func answer4Button(_ sender: AnyObject) {
+        if (answer4Button.titleLabel?.text == "+") {
+            let prevVote = self.didVoteAns.index(of: true)
+            if (prevVote == nil) {
+                answer4Button.setTitle("-", for: .normal)
+                self.voters[3].append(self.loginUserID)
+                DBProvider.instance.answerRef.child(self.answersID[3]).updateChildValues(["votes": (self.votes[3] + 1)])
+                DBProvider.instance.answerRef.child(self.answersID[3]).updateChildValues(["answerVoters": self.voters[3]])
+                updateData(questionID: self.questionID)
+            } else {
+                let userIdx = self.voters[prevVote!].index(of: "\(self.loginUserID)")
+                self.voters[prevVote!].remove(at: userIdx!)
+                answer4Button.setTitle("-", for: .normal)
+                self.voters[3].append(self.loginUserID)
+                DBProvider.instance.answerRef.child(self.answersID[prevVote!]).updateChildValues(["votes": (self.votes[prevVote!] - 1)])
+                DBProvider.instance.answerRef.child(self.answersID[prevVote!]).updateChildValues(["answerVoters": self.voters[prevVote!]])
+                DBProvider.instance.answerRef.child(self.answersID[3]).updateChildValues(["votes": (self.votes[3] + 1)])
+                DBProvider.instance.answerRef.child(self.answersID[3]).updateChildValues(["answerVoters": self.voters[3]])
+                updateData(questionID: self.questionID)
+            }
+        } else {
+            answer4Button.setTitle("+", for: .normal)
+            let index = self.voters[3].index(of: "\(self.loginUserID)")
+            self.voters[3].remove(at: index!)
+            DBProvider.instance.answerRef.child(self.answersID[3]).updateChildValues(["votes": (self.votes[3] - 1)])
+            DBProvider.instance.answerRef.child(self.answersID[3]).updateChildValues(["answerVoters": self.voters[3]])
+            updateData(questionID: self.questionID)
+        }
+
     }
     
     @IBAction func answer5Button(_ sender: AnyObject) {
+        if (answer5Button.titleLabel?.text == "+") {
+            let prevVote = self.didVoteAns.index(of: true)
+            if (prevVote == nil) {
+                answer5Button.setTitle("-", for: .normal)
+                self.voters[4].append(self.loginUserID)
+                DBProvider.instance.answerRef.child(self.answersID[4]).updateChildValues(["votes": (self.votes[4] + 1)])
+                DBProvider.instance.answerRef.child(self.answersID[4]).updateChildValues(["answerVoters": self.voters[4]])
+                updateData(questionID: self.questionID)
+
+            } else {
+                let userIdx = self.voters[prevVote!].index(of: "\(self.loginUserID)")
+                self.voters[prevVote!].remove(at: userIdx!)
+                answer5Button.setTitle("-", for: .normal)
+                self.voters[4].append(self.loginUserID)
+                DBProvider.instance.answerRef.child(self.answersID[prevVote!]).updateChildValues(["votes": (self.votes[prevVote!] - 1)])
+                DBProvider.instance.answerRef.child(self.answersID[prevVote!]).updateChildValues(["answerVoters": self.voters[prevVote!]])
+                DBProvider.instance.answerRef.child(self.answersID[4]).updateChildValues(["votes": (self.votes[4] + 1)])
+                DBProvider.instance.answerRef.child(self.answersID[4]).updateChildValues(["answerVoters": self.voters[4]])
+                updateData(questionID: self.questionID)
+            }
+        } else {
+            answer1Button.setTitle("+", for: .normal)
+            let index = self.voters[4].index(of: "\(self.loginUserID)")
+            self.voters[4].remove(at: index!)
+            DBProvider.instance.answerRef.child(self.answersID[4]).updateChildValues(["votes": (self.votes[4] - 1)])
+            DBProvider.instance.answerRef.child(self.answersID[4]).updateChildValues(["answerVoters": self.voters[4]])
+            updateData(questionID: self.questionID)
+        }
+
     }
     
     @IBAction func backButton(_ sender: AnyObject) {
